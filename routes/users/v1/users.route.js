76ff4bd91e2b155c5f1eaafa0f72b94a069aa964
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-09-25 21:56:51 +0600

    complete

--- routes/users/v1/users.route.js
+++ routes/users/v1/users.route.js
@@ -16,8 +16,12 @@ router.route("/").get(getUsers)
 
 router.route("/").post(addAUser)
 
+
+// Don't need send id from body to update user data
 router.route("/:id").patch(updateAUser)
 
+
+// for bulk update need to send id
 router.route("/bulk-update").patch(bulkUpdate)
 
 router.route("/:id").delete(deleteAUser);
